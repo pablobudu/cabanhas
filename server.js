c67dc5ff028afa25b967efd6d6ff80404f9651cd
pablobudu <paceiton1@gmail.com>
2022-06-08 16:37:06 -0400

    sss

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+import express from 'express'
 import { append } from 'express/lib/response'
 import router from './routes/routes.js'
 const rutas = router
 append.use(router)
+
+
+
+append.listen(3000, ()=>{
+  console.log('Listen 3000');
+})
+
